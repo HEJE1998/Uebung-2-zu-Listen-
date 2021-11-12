cd0491ae55cdb6c3a79e9d7a4620af31243a22ac
HEJE1998 <JeldrikHemme98@web.dem>
2021-11-12 14:09:21 +0100

    Version 3 Programm kommentiert

--- aufgabe1.py
+++ aufgabe1.py
@@ -4,7 +4,7 @@
 
 #Hardware: wird nicht benötigt 
 
-#Version:0.2
+#Version:0.3
 
 #Aufgabe: Schreiben Sie ein Programm, das mithilfe einer for-Schleife alle durch 9 teilbaren Zahlen zwischen zwei zuvor eingegebenen Grenzen
 #in eine Liste schreibt und dann ausgibt.
@@ -13,12 +13,15 @@
 unteregrenze = input('untere Grenze eingeben ')
 oberegrenze = input('obere grenze eingeben ')
 
+#Erstellen einer Variable für die Liste  
 zahlenliste = []
 
+#Erstellen der Liste und das Sortieren duch den Befehl append (hinzufügen von Werten in der) Liste
 for zahlen in range (int(unteregrenze),int(oberegrenze)):
     if zahlen % 9 == 0:
         zahlenliste.append(zahlen)
 
+#Erstellen der Liste zum Ausgeben 
 for liste in zahlenliste:
     print(liste)
 
